000f1f87922ff7ceee1dd3adb12f73e4e592c337
baf62495 <baf62495@aol.com>
2020-01-24 11:06:11 -0500

    server and db setup

---
+++ seeds/seed.logs.sql
@@ -0,0 +1,6 @@
+INSERT INTO logs (content, mood, user_id)
+VALUES
+    ('Having an alright day', 'Neutral', 1),
+    ('Work did not go so well', 'Angry', 1),
+    ('Had an AMAZING date today', 'Very Good', 1),
+    ('Today was a good day', 'Good', 1);
---
+++ seeds/seed.users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (first_name, last_name, username, email)
+VALUES
+    ('Jane', 'Doe', 'jdoe123', 'jdoe123@seed.com');
