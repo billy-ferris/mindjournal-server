c9ad6eba7ef929bf249fdf5c67bd94179e6b438b
baf62495 <baf62495@aol.com>
2020-01-26 21:34:49 -0500

    readying for deploy

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Billy@localhost/mindjournal',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Billy@localhost/mindjournal-test',
 }
